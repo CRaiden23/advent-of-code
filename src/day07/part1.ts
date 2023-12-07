eb2157cfbeaab066c4feeda5b4327e5c9596787f
Steven <VSnake1701@gmail.com>
2023-12-07 08:29:55 -0600

    Day 7 : ifs on ifs

---
+++ src/day07/part1.ts
@@ -0,0 +1,131 @@
+import { parseInput } from '../util/index.js';
+
+type Card =
+  | 'A'
+  | 'K'
+  | 'Q'
+  | 'J'
+  | 'T'
+  | '9'
+  | '8'
+  | '7'
+  | '6'
+  | '5'
+  | '4'
+  | '3'
+  | '2';
+
+const CardStrengths: { [cards in Card]: number } = {
+  A: 12,
+  K: 11,
+  Q: 10,
+  J: 9,
+  T: 8,
+  9: 7,
+  8: 6,
+  7: 5,
+  6: 4,
+  5: 3,
+  4: 2,
+  3: 1,
+  2: 0,
+};
+
+const input = parseInput({ split: { mapper: false } });
+let result = 0;
+
+class Hand {
+  Cards: string;
+  Bid: number;
+  HandMap: Map<string, number>;
+  Rank = 0;
+
+  constructor(cards: string, bid: number, map: Map<string, number>) {
+    this.Cards = cards;
+    this.Bid = bid;
+    this.HandMap = map;
+  }
+
+  determineInitialRank() {
+    const matchingCounts = Array.from(this.HandMap.values());
+    const numSets = matchingCounts.length;
+
+    switch (numSets) {
+      case 1:
+        this.Rank = 6;
+        break;
+      case 2:
+        if (matchingCounts.find((count) => count === 4)) {
+          this.Rank = 5;
+        } else {
+          this.Rank = 4;
+        }
+        break;
+      case 3:
+        if (matchingCounts.find((count) => count === 3)) {
+          this.Rank = 3;
+        } else {
+          this.Rank = 2;
+        }
+        break;
+      case 4:
+        this.Rank = 1;
+        break;
+      default:
+        this.Rank = 0;
+        break;
+    }
+  }
+}
+
+const handleTies = (hands: Hand[]): Hand[] => {
+  hands.sort((a, b) => {
+    if (a.Rank > b.Rank) {
+      return 1;
+    }
+
+    if (a.Rank < b.Rank) {
+      return -1;
+    }
+
+    if (a.Rank === b.Rank) {
+      for (let i = 0; i < a.Cards.length; i++) {
+        if (
+          CardStrengths[a.Cards[i] as Card] > CardStrengths[b.Cards[i] as Card]
+        ) {
+          return 1;
+        } else if (
+          CardStrengths[a.Cards[i] as Card] < CardStrengths[b.Cards[i] as Card]
+        ) {
+          return -1;
+        }
+      }
+    }
+
+    return 0;
+  });
+
+  return hands;
+};
+
+let hands: Hand[] = [];
+input.forEach((line) => {
+  const [cards, bid] = line.split(' ');
+  const hand = new Map<string, number>();
+
+  cards.split('').forEach((card) => {
+    hand.set(card, (hand.get(card) ?? 0) + 1);
+  });
+
+  hands.push(new Hand(cards, Number(bid), hand));
+});
+
+hands.forEach((hand) => hand.determineInitialRank());
+
+hands = handleTies(hands);
+
+hands.forEach((hand, index) => {
+  result += hand.Bid * (index + 1);
+});
+
+export default result;
---
+++ src/day07/part2.ts
@@ -0,0 +1,152 @@
+import { parseInput } from '../util/index.js';
+
+type Card =
+  | 'A'
+  | 'K'
+  | 'Q'
+  | 'T'
+  | '9'
+  | '8'
+  | '7'
+  | '6'
+  | '5'
+  | '4'
+  | '3'
+  | '2'
+  | 'J';
+
+const CardStrengths: { [cards in Card]: number } = {
+  A: 12,
+  K: 11,
+  Q: 10,
+  T: 9,
+  9: 8,
+  8: 7,
+  7: 6,
+  6: 5,
+  5: 4,
+  4: 3,
+  3: 2,
+  2: 1,
+  J: 0,
+};
+
+enum Rank {
+  FiveKind = 6,
+  FourKind = 5,
+  FullHouse = 4,
+  ThreeKind = 3,
+  TwoPair = 2,
+  OnePair = 1,
+  HighCard = 0,
+}
+
+const input = parseInput({ split: { mapper: false } });
+let result = 0;
+
+class Hand {
+  Cards: string;
+  Bid: number;
+  HandMap: Map<string, number>;
+  JokerCount = 0;
+  Rank = 0;
+
+  constructor(
+    cards: string,
+    bid: number,
+    map: Map<string, number>,
+    jokerCount: number
+  ) {
+    this.Cards = cards;
+    this.Bid = bid;
+    this.HandMap = map;
+    this.JokerCount = jokerCount;
+  }
+
+  determineInitialRank() {
+    const matchingCounts = Array.from(this.HandMap.values());
+    const numSets = matchingCounts.length;
+    const hasAtLeastOneJoker = this.JokerCount > 0;
+    const hasTwoJokers = this.JokerCount == 2;
+
+    switch (numSets) {
+      case 1:
+        this.Rank = Rank.FiveKind;
+        break;
+      case 2:
+        if (matchingCounts.find((count) => count === 4)) {
+          this.Rank = hasAtLeastOneJoker ? Rank.FiveKind : Rank.FourKind;
+        } else {
+          this.Rank = hasAtLeastOneJoker ? Rank.FiveKind : Rank.FullHouse;
+        }
+        break;
+      case 3:
+        if (matchingCounts.find((count) => count === 3)) {
+          this.Rank = hasAtLeastOneJoker ? Rank.FourKind : Rank.ThreeKind;
+        } else {
+          if (hasAtLeastOneJoker) {
+            this.Rank = hasTwoJokers ? Rank.FourKind : Rank.FullHouse;
+          } else {
+            this.Rank = Rank.TwoPair;
+          }
+        }
+        break;
+      case 4:
+        this.Rank = hasAtLeastOneJoker ? Rank.ThreeKind : Rank.OnePair;
+        break;
+      default:
+        this.Rank = hasAtLeastOneJoker ? Rank.OnePair : Rank.HighCard;
+        break;
+    }
+  }
+}
+
+const handleTies = (hands: Hand[]): Hand[] => {
+  hands.sort((a, b) => {
+    if (a.Rank > b.Rank) {
+      return 1;
+    }
+
+    if (a.Rank < b.Rank) {
+      return -1;
+    }
+
+    if (a.Rank === b.Rank) {
+      for (let i = 0; i < a.Cards.length; i++) {
+        if (CardStrengths[a.Cards[i] as Card] > CardStrengths[b.Cards[i] as Card]) {
+          return 1;
+        } else if (CardStrengths[a.Cards[i] as Card] < CardStrengths[b.Cards[i] as Card]) {
+          return -1;
+        }
+      }
+    }
+
+    return 0;
+  });
+
+  return hands;
+};
+
+let hands: Hand[] = [];
+input.forEach((line) => {
+  const [cards, bid] = line.split(' ');
+  const hand = new Map<string, number>();
+  let jokerCount = 0;
+
+  cards.split('').forEach((card) => {
+    hand.set(card, (hand.get(card) ?? 0) + 1);
+    if (card === 'J') jokerCount++;
+  });
+
+  hands.push(new Hand(cards, Number(bid), hand, jokerCount));
+});
+
+hands.forEach((hand) => hand.determineInitialRank());
+
+hands = handleTies(hands);
+
+hands.forEach((hand, index) => {
+  result += hand.Bid * (index + 1);
+});
+
+export default result;
